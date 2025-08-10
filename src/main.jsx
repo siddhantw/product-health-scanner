d7b7970f8ffa64bf9dd8fdb214a5274b131546d7
Siddhant Wadhwani <siddhant.wadhwani@newfold.com>
2025-08-10 11:37:45 +0530

    Product Health Scanner - Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './product_health_scanner_react_pwa_demo.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
